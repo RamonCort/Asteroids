1a228a21c64b3d67a0f1cd2ae5fac005ff875b69
RamonCort <a24110188@live.ceti.mx>
2025-06-10 18:17:43 -0600

    Vida_Sprite

--- src/Vida.cpp
+++ src/Vida.cpp
@@ -22,5 +22,32 @@ void Vida::setVidas(int vidas) {
 
 void Vida::draw(sf::RenderWindow& window) {
     window.draw(box);
+    // Dibujar corazones como vidas
+    static sf::Texture corazonTexture;
+    static bool corazonCargado = false;
+    if (!corazonCargado) {
+        corazonCargado = corazonTexture.loadFromFile("assets/images/Corazon.png");
+    }
+    int vidas = 3;
+    try {
+        // Extraer número de vidas del texto
+        std::string str = texto.getString();
+        size_t pos = str.find(":");
+        if (pos != std::string::npos) {
+            vidas = std::stoi(str.substr(pos + 1));
+        }
+    } catch (...) {}
+
+    float x = 30.f;
+    float y = 38.f; // Más arriba que antes (antes 65.f)
+    float separacion = 45.f;
+    for (int i = 0; i < vidas; ++i) {
+        if (corazonCargado) {
+            sf::Sprite corazon(corazonTexture);
+            corazon.setScale(0.08f, 0.08f);
+            corazon.setPosition(x + i * separacion, y);
+            window.draw(corazon);
+        }
+    }
     window.draw(texto);
 }
